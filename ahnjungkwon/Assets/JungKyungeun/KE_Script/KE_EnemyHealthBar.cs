7222d37e23a45d096193cd486dc287e293ee3df1
OxyGerm <33998183+OxyGerm@users.noreply.github.com>
2020-06-14 16:29:37 +0900

    주인공파트 수정

---
+++ ahnjungkwon/Assets/JungKyungeun/KE_Script/KE_EnemyHealthBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KE_EnemyHealthBar : MonoBehaviour
+{
+    public int health;
+    public int maxHealth=3;
+
+    public GameObject healthBarUI;
+    public Slider slider;
+
+    private void Start()
+    {
+        health = maxHealth;
+        slider.value = CalculateHealth();
+
+    }
+
+    private void Update()
+    {
+        slider.value = CalculateHealth();
+
+       
+        
+    }
+
+    float CalculateHealth()
+    {
+        return health / maxHealth;
+    }
+}
---
+++ ahnjungkwon/Assets/KwonTaeHoon/Scripts/bullet_3.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_3 : MonoBehaviour
+{
+    public float DestroyTime = 2.0f;
+    public float bulletSpeed = 1.0f;
+
+    [SerializeField]
+    private ParticleSystem hitParticle;
+
+    void Start()
+    {
+        Destroy(gameObject, DestroyTime);
+    }
+
+    void Update()
+    {
+        //프레임마다 오브젝트를 로컬좌표상에서 앞으로 1의 힘만큼 날아가라
+        transform.Translate(Vector3.forward * bulletSpeed);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Particle Played");
+        other.GetComponent<KE_Dragon>().OnDamageProcess();
+        hitParticle.Play();
+
+        Destroy(gameObject, 0.3f);
+
+        //if (other.tag == "Enemy")
+        //{
+        //    Debug.Log("Particle Played");
+        //    other.GetComponent<KE_Dragon>().OnDamageProcess();
+        //    hitParticle.Play();
+            
+        //    Destroy(gameObject, 0.3f);
+        //}
+        //else if(other.tag == "Walls")
+        //{
+        //    hitParticle.Play();
+        //}
+
+        
+    }
+
+
+}
